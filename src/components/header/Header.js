9b7b4248101ad5e283ca852a1aaa5afda5e3e36b
Luis Geniole <luis.geniole@gmail.com>
2019-10-27 19:48:01 -0300

    finish header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import facebookLogo from './assets/facebookLogo.svg';
+import profileLogo from './assets/profileLogo.svg';
 
 import './Header.css';
 
@@ -8,7 +9,9 @@ class Header extends Component {
   render() {
     return (
       <div class="header">
-        <img src={facebookLogo} />
+        <img src={facebookLogo} class="facebookLogo" />
+        <h4 class="profile">My Profile</h4>
+        <img src={profileLogo} class="profileLogo" />
       </div>
     );
   }
